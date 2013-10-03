1a5fc715d926ce6e07c7cfd1002ffac53475c234
Ivan Storck <ivanoats@gmail.com>
2013-10-03 14:57:56 -0700

    simplified ubuntu rbenv install steps

--- scripts/install_rbenv_ubuntu.sh
+++ scripts/install_rbenv_ubuntu.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev libruby1.9.1 libreadline-dev git-core build-essential vim sqlite3 libsqlite3-dev 
+sudo apt-get install -y zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev libruby1.9.1 libreadline-dev git-core build-essential vim sqlite3 libsqlite3-dev libqtwebkit-dev g++
+# install nodejs as a javascript runtime, current version
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
 cd ~
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
@@ -14,11 +19,5 @@ rbenv rehash
 rbenv global 2.0.0-p247
 gem install bundler
 rbenv rehash
-sudo apt-get install -y libqtwebkit-dev
-sudo apt-get install -y g++
 gem install capybara-webkit
-# install nodejs as a javascript runtime, current version
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
+rbenv rehash
